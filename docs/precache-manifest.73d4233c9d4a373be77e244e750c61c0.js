8a8874ac246cdeaf84eabc60e3905a373b6e2530
tmtakashi <tmtakashi7@gmail.com>
2019-11-18 08:38:46 +0900

    few fixes

---
+++ docs/precache-manifest.73d4233c9d4a373be77e244e750c61c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25b0530c4fcf44438aae839a599f8eb",
+    "url": "/react-tinder/index.html"
+  },
+  {
+    "revision": "ad3b9ea72864764732de",
+    "url": "/react-tinder/static/css/main.8d1ddc6a.chunk.css"
+  },
+  {
+    "revision": "5b1d20e9bdca9254573b",
+    "url": "/react-tinder/static/js/2.9d2b55dc.chunk.js"
+  },
+  {
+    "revision": "ad3b9ea72864764732de",
+    "url": "/react-tinder/static/js/main.8d68ee41.chunk.js"
+  },
+  {
+    "revision": "dd2f92bfb8c058abdfc6",
+    "url": "/react-tinder/static/js/runtime-main.06e9b1d8.js"
+  }
+]);
